64d04db77db51ecd1e948b66c2006b0fa950c345
Sven Peldszus <sven.peldszus@rub.de>
2024-06-01 15:27:03 +0200

    Removed UMLsec High

--- implementation/org.gravity.umlsecrt.agent/src/carisma/rt/instrument/RTHelper.java
+++ implementation/org.gravity.umlsecrt.agent/src/carisma/rt/instrument/RTHelper.java
@@ -3,7 +3,6 @@ package carisma.rt.instrument;
 import java.lang.annotation.Annotation;
 import java.util.List;
 
-import org.gravity.security.annotations.requirements.High;
 import org.gravity.security.annotations.requirements.Integrity;
 import org.gravity.security.annotations.requirements.Secrecy;
 
@@ -16,7 +15,7 @@ import javassist.NotFoundException;
 import javassist.expr.FieldAccess;
 
 public class RTHelper {
-	
+
 	private RTHelper() {
 		// This class shouldn't be instantiated
 	}
@@ -24,22 +23,21 @@ public class RTHelper {
 	/**
 	 * Creates an early return string which can be inserted into the instrumented
 	 * code
-	 * 
+	 *
 	 * @param memeber        The member for which the early return is needed
 	 * @param annotationType The type of annotation for which the early return is
 	 *                       needed
 	 * @return An early return string
 	 */
-	static String getEarlyReturn(CtMember member, CtClass owner, Class<? extends Annotation> annotationType)
+	static String getEarlyReturn(final CtMember member, final CtClass owner,
+			final Class<? extends Annotation> annotationType)
 			throws ClassNotFoundException, NotFoundException {
 		String earlyReturn;
-		Object annotation = member.getAnnotation(annotationType);
+		final Object annotation = member.getAnnotation(annotationType);
 		if (Secrecy.class.equals(annotationType)) {
 			earlyReturn = ((Secrecy) annotation).earlyReturn();
 		} else if (Integrity.class.equals(annotationType)) {
 			earlyReturn = ((Integrity) annotation).earlyReturn();
-		} else if (High.class.equals(annotationType)) {
-			earlyReturn = ((High) annotation).earlyReturn();
 		} else {
 			return null;
 		}
@@ -59,86 +57,86 @@ public class RTHelper {
 	/**
 	 * Creates an early return string which can be inserted into the instrumented
 	 * code
-	 * 
+	 *
 	 * @param owner       The class owning the member with the member with an early
 	 *                    return
 	 * @param type        The expected type of the early return value
 	 * @param earlyReturn The early return value from the annotation
 	 * @return An early return string
 	 */
-	static String getEarlyReturn(CtClass owner, CtClass type, String earlyReturn) {
+	static String getEarlyReturn(final CtClass owner, final CtClass type, final String earlyReturn) {
 		if (earlyReturn == null || earlyReturn.length() == 0) {
 			return null;
-		} else if ("null".equalsIgnoreCase(earlyReturn)) {
+		}
+		if ("null".equalsIgnoreCase(earlyReturn)) {
 			return "null";
+		} else if ("void".equalsIgnoreCase(earlyReturn)) {
+			return "";
+		} else if ("true".equalsIgnoreCase(earlyReturn)) {
+			return Boolean.toString(true);
+		} else if ("false".equalsIgnoreCase(earlyReturn)) {
+			return Boolean.toString(false);
+		} else if ('"' == earlyReturn.charAt(0) && earlyReturn.charAt(earlyReturn.length() - 1) == '"') {
+			return earlyReturn;
 		} else {
-			if ("void".equalsIgnoreCase(earlyReturn)) {
-				return "";
-			} else if ("true".equalsIgnoreCase(earlyReturn)) {
-				return Boolean.toString(true);
-			} else if ("false".equalsIgnoreCase(earlyReturn)) {
-				return Boolean.toString(false);
-			} else if ('"' == earlyReturn.charAt(0) && earlyReturn.charAt(earlyReturn.length() - 1) == '"') {
-				return earlyReturn;
-			} else {
-				CtMethod method;
-				try {
-					method = owner.getDeclaredMethod(earlyReturn);
-					if (method != null && method.getParameterTypes().length == 0
-							&& method.getReturnType().equals(type)) {
-						return method.getName() + "()";
+			CtMethod method;
+			try {
+				method = owner.getDeclaredMethod(earlyReturn);
+				if (method != null && method.getParameterTypes().length == 0
+						&& method.getReturnType().equals(type)) {
+					return method.getName() + "()";
 
-					}
-				} catch (NotFoundException e) {
-					e.printStackTrace();
 				}
-				try {
-					return Integer.toString(Integer.parseInt(earlyReturn));
-				} catch (NumberFormatException e) {
-					printAgentError(e);
-				}
-				try {
-					return Double.toString(Double.parseDouble(earlyReturn));
-				} catch (NumberFormatException e) {
-					printAgentError(e);
-				}
-				System.err.println("Didn't found counter measure: " + earlyReturn);
-				return null;
+			} catch (final NotFoundException e) {
+				e.printStackTrace();
 			}
+			try {
+				return Integer.toString(Integer.parseInt(earlyReturn));
+			} catch (final NumberFormatException e) {
+				printAgentError(e);
+			}
+			try {
+				return Double.toString(Double.parseDouble(earlyReturn));
+			} catch (final NumberFormatException e) {
+				printAgentError(e);
+			}
+			System.err.println("Didn't found counter measure: " + earlyReturn);
+			return null;
 		}
 	}
 
 	/**
 	 * Builds the signature of a field
-	 * 
+	 *
 	 * @param field The field
 	 * @return The fields signature
 	 */
-	public static String getSignature(CtField field) {
-		StringBuilder signature = new StringBuilder(field.getDeclaringClass().getName());
+	public static String getSignature(final CtField field) {
+		final StringBuilder signature = new StringBuilder(field.getDeclaringClass().getName());
 		signature.append('.');
 		signature.append(field.getName());
 		try {
 			signature.append(':');
 			signature.append(field.getType().getSimpleName());
-		} catch (NotFoundException e) {
+		} catch (final NotFoundException e) {
 			printAgentError(e);
 		}
 		return signature.toString();
 	}
 
-	public static String getFieldSignature(FieldAccess access) {
-		StringBuilder signature = new StringBuilder(access.getClassName()).append('.').append(access.getFieldName())
+	public static String getFieldSignature(final FieldAccess access) {
+		final StringBuilder signature = new StringBuilder(access.getClassName()).append('.')
+				.append(access.getFieldName())
 				.append(':');
 		String type = access.getSignature();
 		if (type.charAt(type.length() - 1) == ';') {
 			type = type.substring(0, type.length() - 1);
 		}
-		char firstChar = type.charAt(0);
+		final char firstChar = type.charAt(0);
 		if (type.length() == 1) {
 			return appendPrimitiveType(signature, firstChar).toString();
 		}
-		String substring = type.substring(1);
+		final String substring = type.substring(1);
 		if ('L' == firstChar) {
 			return signature.append(substring.replace('/', '.')).toString();
 		}
@@ -156,37 +154,37 @@ public class RTHelper {
 	 * @param firstChar
 	 * @return
 	 */
-	private static StringBuilder appendPrimitiveType(StringBuilder signature, char firstChar) {
+	private static StringBuilder appendPrimitiveType(final StringBuilder signature, final char firstChar) {
 		switch (firstChar) {
-		case 'Z':
-			return signature.append("boolean");
-		case 'B':
-			return signature.append("byte");
-		case 'C':
-			return signature.append("char");
-		case 'S':
-			return signature.append("short");
-		case 'I':
-			return signature.append("int");
-		case 'J':
-			return signature.append("long");
-		case 'F':
-			return signature.append("float");
-		case 'D':
-			return signature.append("double");
-		default:
-			throw new IllegalStateException("Unknown primitive type: " + firstChar);
+			case 'Z':
+				return signature.append("boolean");
+			case 'B':
+				return signature.append("byte");
+			case 'C':
+				return signature.append("char");
+			case 'S':
+				return signature.append("short");
+			case 'I':
+				return signature.append("int");
+			case 'J':
+				return signature.append("long");
+			case 'F':
+				return signature.append("float");
+			case 'D':
+				return signature.append("double");
+			default:
+				throw new IllegalStateException("Unknown primitive type: " + firstChar);
 		}
 	}
 
 	/**
 	 * Prints the exception to the console
-	 * 
+	 *
 	 * @param error The thrown exception
 	 */
-	public static void printAgentError(Exception error) {
+	public static void printAgentError(final Exception error) {
 		String location = "Couldn't get location";
-		Thread currentThread = Thread.currentThread();
+		final Thread currentThread = Thread.currentThread();
 		if (currentThread != null) {
 			final StackTraceElement[] stackTrace = currentThread.getStackTrace();
 			if (stackTrace.length > 1) {
@@ -198,14 +196,14 @@ public class RTHelper {
 
 	/**
 	 * Prints the exception to the console
-	 * 
+	 *
 	 * @param error The thrown exception
 	 */
-	public static void printAgentError(Exception error, String location) {
+	public static void printAgentError(final Exception error, final String location) {
 		if (error != null) {
 			System.out.println("[AGENT] ERROR (" + error.getClass().getSimpleName() + " at \"" + location + "\"): "
 					+ error.getLocalizedMessage());
-			for (StackTraceElement s : error.getStackTrace()) {
+			for (final StackTraceElement s : error.getStackTrace()) {
 				System.out.println("[AGENT] \t " + s.toString());
 			}
 		} else {
@@ -213,18 +211,18 @@ public class RTHelper {
 		}
 	}
 
-	static boolean containsSignature(String fieldSignature, String[] values) {
-		for (int i = 0; i < values.length; i++) {
-			if (fieldSignature.endsWith(values[i])) {
+	static boolean containsSignature(final String fieldSignature, final String[] values) {
+		for (final String value : values) {
+			if (fieldSignature.endsWith(value)) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	static boolean containsSignature(String fieldSignature, List<String> values) {
-		for (int i = 0; i < values.size(); i++) {
-				if (fieldSignature.endsWith(values.get(i))) {
+	static boolean containsSignature(final String fieldSignature, final List<String> values) {
+		for (final String value : values) {
+			if (fieldSignature.endsWith(value)) {
 				return true;
 			}
 		}
--- implementation/org.gravity.umlsecrt.editor/src/carisma/rt/editor/CompilationParticipant.java
+++ implementation/org.gravity.umlsecrt.editor/src/carisma/rt/editor/CompilationParticipant.java
@@ -3,7 +3,6 @@ package carisma.rt.editor;
 import java.util.Collections;
 import java.util.Stack;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.IAnnotation;
@@ -18,7 +17,6 @@ import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.Signature;
 import org.eclipse.jdt.core.compiler.BuildContext;
-import org.gravity.security.annotations.requirements.High;
 import org.gravity.security.annotations.requirements.Integrity;
 import org.gravity.security.annotations.requirements.Secrecy;
 
@@ -37,15 +35,15 @@ public class CompilationParticipant extends org.eclipse.jdt.core.compiler.Compil
 	@Override
 	public void processAnnotations(final BuildContext[] files) {
 		for (final BuildContext file : files) {
-			final IFile iFile = file.getFile();
-			final ICompilationUnit cu = (ICompilationUnit) JavaCore.create(iFile);
+			final var iFile = file.getFile();
+			final var cu = (ICompilationUnit) JavaCore.create(iFile);
 			try {
 				for (final IType type : cu.getTypes()) {
 					for (final IMethod child : type.getMethods()) {
-						process(child.getAnnotations(), type);
+						this.process(child.getAnnotations(), type);
 					}
 					for (final IField child : type.getFields()) {
-						process(child.getAnnotations(), type);
+						this.process(child.getAnnotations(), type);
 					}
 				}
 			} catch (final JavaModelException e) {
@@ -57,20 +55,17 @@ public class CompilationParticipant extends org.eclipse.jdt.core.compiler.Compil
 	private void process(final IAnnotation[] annotations, final IType type) {
 		for (final IAnnotation annotation : annotations) {
 			try {
-				final String[][] resolved = type.resolveType(annotation.getElementName());
+				final var resolved = type.resolveType(annotation.getElementName());
 				if (resolved == null) {
 					continue;
 				}
 				for (final String[] element : resolved) {
-					final String earlyReturn = getEarlyReturn(element[0] + '.' + element[1], annotation);
-					if (earlyReturn == null) {
+					final var earlyReturn = this.getEarlyReturn(element[0] + '.' + element[1], annotation);
+					if ((earlyReturn == null) || (earlyReturn.length() == 0)) {
 						continue;
 					}
-					if (earlyReturn.length() == 0) {
-						continue;
-					}
-					final String typeSignature = getTypeSignature(annotation);
-					String typeName = Signature.toString(typeSignature);
+					final var typeSignature = this.getTypeSignature(annotation);
+					var typeName = Signature.toString(typeSignature);
 
 					if ("void".equalsIgnoreCase(earlyReturn)) {
 					} else if ("null".equalsIgnoreCase(earlyReturn)) {
@@ -80,15 +75,17 @@ public class CompilationParticipant extends org.eclipse.jdt.core.compiler.Compil
 								|| Signature.SIG_DOUBLE.equals(typeSignature)
 								|| Signature.SIG_CHAR.equals(typeSignature)
 								|| Signature.SIG_BOOLEAN.equals(typeSignature)) {
-							createMarker(type, annotation, "The value  \"null\" cannot be used with primitive types.");
+							this.createMarker(type, annotation,
+									"The value  \"null\" cannot be used with primitive types.");
 						}
 					} else if ("true".equalsIgnoreCase(earlyReturn) || "false".equalsIgnoreCase(earlyReturn)) {
 						if (!Signature.SIG_BOOLEAN.equals(typeName)) {
-							createMarker(type, annotation,
+							this.createMarker(type, annotation,
 									"The method \"" + earlyReturn + "\" has to return a boolean value.");
 						}
-					} else if (('"' == earlyReturn.charAt(0)) && (earlyReturn.charAt(earlyReturn.length() - 1) == '"')) {
-						boolean isString = false;
+					} else if (('"' == earlyReturn.charAt(0))
+							&& (earlyReturn.charAt(earlyReturn.length() - 1) == '"')) {
+						var isString = false;
 						for (final String[] entry : type.resolveType(typeName)) {
 							if ("String".equals(entry[1]) && "java.lang".equals(entry[0])) {
 								isString = true;
@@ -96,35 +93,38 @@ public class CompilationParticipant extends org.eclipse.jdt.core.compiler.Compil
 							}
 						}
 						if (!isString) {
-							createMarker(type, annotation, "The \"java.lang.String\" value \"" + earlyReturn
+							this.createMarker(type, annotation, "The \"java.lang.String\" value \"" + earlyReturn
 									+ "\" is not compatible with the return type \"" + typeName + "\".");
 						}
 					} else {
 						try {
-							final int value = Integer.parseInt(earlyReturn);
+							final var value = Integer.parseInt(earlyReturn);
 							if (!Signature.SIG_INT.equals(typeSignature)
 									&& (!Signature.SIG_BYTE.equals(typeSignature) || (-128 > value) || (value > 127))
-									&& (!Signature.SIG_CHAR.equals(typeSignature) || (Character.MIN_VALUE > value) || (value > Character.MAX_VALUE))) {
-								createMarker(type, annotation, "The int value \"" + earlyReturn
+									&& (!Signature.SIG_CHAR.equals(typeSignature) || (Character.MIN_VALUE > value)
+											|| (value > Character.MAX_VALUE))) {
+								this.createMarker(type, annotation, "The int value \"" + earlyReturn
 										+ "\" is not compatible with the return type \"" + typeName + "\".");
 							}
 						} catch (final NumberFormatException e0) {
 							try {
-								final double value = Double.parseDouble(earlyReturn);
+								final var value = Double.parseDouble(earlyReturn);
 								if (!Signature.SIG_DOUBLE.equals(typeSignature)
-										&& (!Signature.SIG_FLOAT.equals(typeSignature) || (Float.MIN_VALUE > value) || (value > Float.MAX_VALUE))) {
-									createMarker(type, annotation, "The double value \"" + earlyReturn
+										&& (!Signature.SIG_FLOAT.equals(typeSignature) || (Float.MIN_VALUE > value)
+												|| (value > Float.MAX_VALUE))) {
+									this.createMarker(type, annotation, "The double value \"" + earlyReturn
 											+ "\" is not compatible with the return type \"" + typeName + "\".");
 								}
 							} catch (final NumberFormatException e1) {
-								if (!isShort(type, annotation, earlyReturn, typeSignature, typeName) && !isLong(type, annotation, earlyReturn, typeSignature, typeName)) {
+								if (!this.isShort(type, annotation, earlyReturn, typeSignature, typeName)
+										&& !this.isLong(type, annotation, earlyReturn, typeSignature, typeName)) {
 									IMethod foundMethod = null;
 									for (final IMethod m : type.getMethods()) {
 										if (m.getElementName().equals(earlyReturn)) {
 											foundMethod = m;
 											if (m.getNumberOfParameters() != 0) {
 												try {
-													createMarker(type, annotation,
+													this.createMarker(type, annotation,
 															"Only methods without parameters are allowed for as early return values.");
 												} catch (final CoreException e) {
 													e.printStackTrace();
@@ -133,40 +133,41 @@ public class CompilationParticipant extends org.eclipse.jdt.core.compiler.Compil
 										}
 									}
 									if ((foundMethod != null) && !typeName.toLowerCase().equals("void")) {
-										boolean correctReturnType = false;
-										typeName = replacePrimitiveWithObject(typeName);
-										final String[][] resolvedNeededTypes = type.resolveType(typeName);
-										final String unresolvedReturnType = Signature.toString(foundMethod.getReturnType());
+										var correctReturnType = false;
+										typeName = this.replacePrimitiveWithObject(typeName);
+										final var resolvedNeededTypes = type.resolveType(typeName);
+										final var unresolvedReturnType = Signature
+												.toString(foundMethod.getReturnType());
 										if (!unresolvedReturnType.toLowerCase().contentEquals("void")) {
-											final String[][] resolvedReturnTypes = type
-													.resolveType(replacePrimitiveWithObject(
+											final var resolvedReturnTypes = type
+													.resolveType(this.replacePrimitiveWithObject(
 															unresolvedReturnType));
 											for (final String[] neededEntry : resolvedNeededTypes) {
-												final String neededTypeSignature = neededEntry[0] + '.' + neededEntry[1];
+												final var neededTypeSignature = neededEntry[0] + '.'
+														+ neededEntry[1];
 												for (final String[] returnEntry : resolvedReturnTypes) {
-													final Stack<String> returnTypes = new Stack<>();
+													final var returnTypes = new Stack<String>();
 													returnTypes
-													.add('Q' + returnEntry[0] + '.' + returnEntry[1] + ';');
+															.add('Q' + returnEntry[0] + '.' + returnEntry[1] + ';');
 													while (!returnTypes.isEmpty()) {
-														final String pop = returnTypes.pop();
-														final String returnTypeSignature = Signature.toString(pop)
+														final var pop = returnTypes.pop();
+														final var returnTypeSignature = Signature.toString(pop)
 																.replaceAll("<.*>", "");
 														if (returnTypeSignature.equals(neededTypeSignature)) {
 															correctReturnType = true;
 															break;
-														} else {
-															final IType returnType = type.getJavaProject()
-																	.findType(returnTypeSignature);
-															final String superclassTypeSignature = returnType
-																	.getSuperclassTypeSignature();
-															if (superclassTypeSignature != null) {
-																returnTypes.add(superclassTypeSignature);
-															} else {
-																returnTypes.add("Ljava.lang.Object;");
-															}
-															Collections.addAll(returnTypes, returnType
-																	.getSuperInterfaceTypeSignatures());
 														}
+														final var returnType = type.getJavaProject()
+																.findType(returnTypeSignature);
+														final var superclassTypeSignature = returnType
+																.getSuperclassTypeSignature();
+														if (superclassTypeSignature != null) {
+															returnTypes.add(superclassTypeSignature);
+														} else {
+															returnTypes.add("Ljava.lang.Object;");
+														}
+														Collections.addAll(returnTypes, returnType
+																.getSuperInterfaceTypeSignatures());
 													}
 													if (correctReturnType) {
 														break;
@@ -178,12 +179,12 @@ public class CompilationParticipant extends org.eclipse.jdt.core.compiler.Compil
 											}
 										}
 										if (!correctReturnType) {
-											createMarker(type, annotation, "The method \"" + earlyReturn
+											this.createMarker(type, annotation, "The method \"" + earlyReturn
 													+ "\" has to return an instance of \"" + typeName + "\".");
 										}
 									} else {
 										try {
-											createMarker(type, annotation, "There is no method with the name \""
+											this.createMarker(type, annotation, "There is no method with the name \""
 													+ earlyReturn + "\" in this class.");
 										} catch (final CoreException e) {
 											e.printStackTrace();
@@ -204,19 +205,21 @@ public class CompilationParticipant extends org.eclipse.jdt.core.compiler.Compil
 	}
 
 	private String getTypeSignature(final IAnnotation annotation) throws JavaModelException {
-		final IJavaElement parent = annotation.getParent();
+		final var parent = annotation.getParent();
 		if (parent.getElementType() == IJavaElement.METHOD) {
 			return ((IMethod) parent).getReturnType();
-		} else if (parent.getElementType() == IJavaElement.FIELD) {
+		}
+		if (parent.getElementType() == IJavaElement.FIELD) {
 			return ((IField) parent).getTypeSignature();
 		}
 		throw new RuntimeException();
 	}
 
-	private String getEarlyReturn(final String fullyQualifiedName, final IAnnotation annotation) throws JavaModelException {
-		if (fullyQualifiedName.equals(Secrecy.class.getName()) || fullyQualifiedName.equals(Integrity.class.getName())
-				|| fullyQualifiedName.equals(High.class.getName())) {
-			final IMemberValuePair[] pairs = annotation.getMemberValuePairs();
+	private String getEarlyReturn(final String fullyQualifiedName, final IAnnotation annotation)
+			throws JavaModelException {
+		if (fullyQualifiedName.equals(Secrecy.class.getName())
+				|| fullyQualifiedName.equals(Integrity.class.getName())) {
+			final var pairs = annotation.getMemberValuePairs();
 			if (pairs.length != 0) {
 				for (final IMemberValuePair pair : pairs) {
 					if (pair.getMemberName().equals("earlyReturn")) {
@@ -229,15 +232,16 @@ public class CompilationParticipant extends org.eclipse.jdt.core.compiler.Compil
 		return null;
 	}
 
-	private boolean isLong(final IType type, final IAnnotation annotation, final String earlyReturn, final String typeSignature,
+	private boolean isLong(final IType type, final IAnnotation annotation, final String earlyReturn,
+			final String typeSignature,
 			final String typeName) {
 
 		try {
-			final char last = earlyReturn.charAt(earlyReturn.length() - 1);
+			final var last = earlyReturn.charAt(earlyReturn.length() - 1);
 			if ((last == 'l') || (last == 'L')) {
-				final long value = Long.parseLong(earlyReturn.substring(0, earlyReturn.length() - 1));
+				Long.parseLong(earlyReturn.substring(0, earlyReturn.length() - 1));
 				if (!Signature.SIG_LONG.equals(typeSignature)) {
-					createMarker(type, annotation, "The long value \"" + earlyReturn
+					this.createMarker(type, annotation, "The long value \"" + earlyReturn
 							+ "\" is not compatible with the return type \"" + typeName + "\".");
 				}
 				return true;
@@ -250,13 +254,14 @@ public class CompilationParticipant extends org.eclipse.jdt.core.compiler.Compil
 		return false;
 	}
 
-	private boolean isShort(final IType type, final IAnnotation annotation, final String earlyReturn, final String typeSignature,
+	private boolean isShort(final IType type, final IAnnotation annotation, final String earlyReturn,
+			final String typeSignature,
 			final String typeName) {
 
 		try {
 			final long value = Short.parseShort(earlyReturn);
 			if (!Signature.SIG_SHORT.equals(typeSignature)) {
-				createMarker(type, annotation, "The short value \"" + earlyReturn
+				this.createMarker(type, annotation, "The short value \"" + earlyReturn
 						+ "\" is not compatible with the return type \"" + typeName + "\".");
 			}
 			return true;
@@ -271,11 +276,11 @@ public class CompilationParticipant extends org.eclipse.jdt.core.compiler.Compil
 
 	private void createMarker(final IType type, final IAnnotation annotation, final String message)
 			throws CoreException, JavaModelException {
-		final IMarker marker = type.getResource().createMarker("org.gravity.carisma.rt.markers");
+		final var marker = type.getResource().createMarker("org.gravity.carisma.rt.markers");
 		marker.setAttribute(IMarker.MESSAGE, message);
-		final int offset = annotation.getNameRange().getOffset();
+		final var offset = annotation.getNameRange().getOffset();
 		marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
-		final long line = type.getCompilationUnit().getSource().substring(0, offset).chars()
+		final var line = type.getCompilationUnit().getSource().substring(0, offset).chars()
 				.filter(w -> ((w == '\n') || (w == '\r'))).count();
 		marker.setAttribute(IMarker.LINE_NUMBER, (int) line + 1);
 		marker.setAttribute(IMarker.CHAR_START, offset);
